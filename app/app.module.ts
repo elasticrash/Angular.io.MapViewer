850159820b64226187749b09cbcb8d2e050215f1
SKouroupis <stefanos.kouroupis@dottedeyes.com>
2016-10-18 13:22:48 +0100

    toolbar and map placeholder

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from '@angular/material';
 import { AppComponent } from './components/app.component';
 import { ToolbarElement } from './components/toolbar.element';
+import { LeafletElement } from './components/leaflet.element';
 
 @NgModule({
   imports: [BrowserModule, MaterialModule.forRoot()],
-  declarations: [AppComponent, ToolbarElement],
+  declarations: [AppComponent, ToolbarElement, LeafletElement],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ app/components/leaflet.element.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'leaf-element',
+  templateUrl: 'app/templates/leaflet.element.html'
+})
+export class LeafletElement { }
