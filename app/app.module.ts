900a52c2b271705fae8a6456b5bf8104987a3bca
SKouroupis <stefanos.kouroupis@dottedeyes.com>
2016-10-21 17:09:36 +0100

    developing

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from '@angular/material';
 import { AppComponent } from './components/app.component';
 import { ToolbarElement } from './components/toolbar.element';
-import { CandTLeafletComponent  } from 'angular2.leaflet.components';
+import { CandTLeafletComponent } from 'angular2.leaflet.components';
+import { CandTLeafletService } from 'angular2.leaflet.components';
 
 @NgModule({
-  imports: [BrowserModule, MaterialModule.forRoot() ],
+  imports: [BrowserModule, MaterialModule.forRoot()],
   declarations: [AppComponent, ToolbarElement, CandTLeafletComponent],
+  providers: [CandTLeafletService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
