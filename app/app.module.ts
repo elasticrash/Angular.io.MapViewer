d41c39ba59ac9ab78ec4b7cd504343c769a5dc38
stefano kitano <S.Kouroupis@gmail.com>
2016-10-22 00:28:05 +0100

    layer component working

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './components/app.component';
 import { ToolbarElement } from './components/toolbar.element';
 import { CandTLeafletComponent  } from 'angular2.leaflet.components';
 
+
 @NgModule({
   imports: [BrowserModule, MaterialModule.forRoot() ],
   declarations: [AppComponent, ToolbarElement, CandTLeafletComponent],
