b3a509433e7a86b0f38dc56f997b1e37c8970908
stefano kitano <S.Kouroupis@gmail.com>
2016-10-27 00:50:32 +0100

    clearing useless stuff

---
+++ example/node_modules/angular2.leaflet.components/models/path.d.ts
@@ -0,0 +1,16 @@
+export declare class path {
+    constructor(pathInfo: any);
+    stroke: boolean;
+    color: string;
+    weight: number;
+    opacity: number;
+    lineCap: string;
+    lineJoin: string;
+    dashArray: string;
+    dashOffset: string;
+    fill: boolean;
+    fillColor: string;
+    fillOpacity: number;
+    fillRule: string;
+    className: string;
+}
