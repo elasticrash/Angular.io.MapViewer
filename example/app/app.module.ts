c25a854e4596df9f6791aa5f5338df5ff86c2c6b
SKouroupis <stefanos.kouroupis@dottedeyes.com>
2016-11-08 13:41:44 +0000

    back to old commit

---
+++ example/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
+import { AppComponent } from './components/app.component';
+import { ToolbarElement } from './components/toolbar.element';
+import { CandTLeafletComponent } from 'angular2.leaflet.components';
+import { CandTLeafletService } from 'angular2.leaflet.components';
+
+
+@NgModule({
+  imports: [BrowserModule, MaterialModule.forRoot()],
+  declarations: [AppComponent, ToolbarElement, CandTLeafletComponent],
+  providers: [CandTLeafletService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ example/systemjs.config.js
@@ -0,0 +1,75 @@
+/**
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+  System.config({
+    transpiler: 'typescript',
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: 'dist/Angular.io.MapViewer/app',
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      // other libraries
+      'rxjs': 'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      '@angular/material': 'npm:@angular/material/material.umd.js',
+      'leaflet': 'npm:leaflet',
+      'angular2.leaflet.components': 'npm:angular2.leaflet.components',
+      //typescript
+      "ts": "npm:plugin-typescript/lib/",
+      "typescript": "npm:typescript/lib/"
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      },
+      'angular-in-memory-web-api': {
+        main: './index.js',
+        defaultExtension: 'js'
+      },
+      'angular2.leaflet.components': {
+        main: './index',
+        defaultExtension: 'js',
+        meta: {
+          "format": "register"
+        }
+      },
+      'leaflet': {
+        main: './dist/leaflet.js',
+        defaultExtension: 'js',
+        meta: {
+          './dist/leaflet.css': { loader: 'css' }
+        }
+      },
+      "ts": {
+        "main": "plugin.js"
+      },
+      "typescript": {
+        "main": "typescript.js",
+        "meta": {
+          "lib/typescript.js": {
+            "exports": "ts"
+          }
+        }
+      }
+    }
+  });
+})(this);
