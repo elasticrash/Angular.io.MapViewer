6844f5d98b46a07435363e1a3ad446b7dbbaeb07
stefano kitano <S.Kouroupis@gmail.com>
2017-01-31 00:27:25 +0000

    resolver

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './components/app.component';
 import { AppComponent1 } from './components/app.component.1';
 import { AppComponent2 } from './components/app.component.2';
 import { AppComponent3 } from './components/app.component.3';
+import { AppComponent4 } from './components/app.component.4';
 import { RandomPoints } from './components/random.points';
 import { Simple } from './components/simple';
 import { ToolbarElement } from './components/toolbar.element';
@@ -13,6 +14,8 @@ import { CandTLeafletService } from 'angular2.leaflet.components';
 import { RouterModule } from '@angular/router';
 import { GeneratePoints } from './experiments/generate.points';
 import { CoordinateControl } from './components/coordinates';
+import { GamePoints } from './game/game';
+import { GameResolver } from './game/game.resolver';
 
 
 @NgModule({
@@ -21,13 +24,19 @@ import { CoordinateControl } from './components/coordinates';
       { path: 'mm-map', component: AppComponent1 },
       { path: '', component: AppComponent2 },
       { path: 'prj', component: AppComponent3 },
+      {
+        path: 'game', component: AppComponent4,
+        resolve: {
+          countries: GameResolver
+        }
+      },
       { path: 'random', component: RandomPoints },
       { path: 'simple', component: Simple }
     ]),
   ],
-  declarations: [AppComponent, AppComponent1, AppComponent2, AppComponent3,
+  declarations: [AppComponent, AppComponent1, AppComponent2, AppComponent3, AppComponent4, GamePoints,
     RandomPoints, GeneratePoints, Simple, ToolbarElement, CandTLeafletComponent, CoordinateControl],
-  providers: [CandTLeafletService],
+  providers: [CandTLeafletService, GameResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ app/game/game.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Http, Response, Headers, RequestOptions, Request, RequestMethod, ResponseContentType } from '@angular/http';
+
+
+@Component({
+    selector: 'game-points',
+    templateUrl: 'app/game/game.html'
+})
+
+export class GamePoints {
+    
+}
